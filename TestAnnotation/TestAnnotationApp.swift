2ba46116b1bc408ed6e8ddd17594724c5ac5a297
Florianto <ftousch@ippon.fr>
2023-01-03 22:39:55 +0100

    Initial Commit

---
+++ TestAnnotation/TestAnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAnnotationApp.swift
+//  TestAnnotation
+//
+//  Created by Florian on 03/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
